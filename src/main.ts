36ae94327bacf69b1bcb42797984ef606ec1a224
Shingo Yamada <uzuranoie@gmail.com>
2023-03-27 22:44:01 +0900

    Installed quasar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
+import { Quasar } from 'quasar'
 import { createPinia } from 'pinia'
+import quasarLang from 'quasar/lang/ja'
+
+// Import icon libraries
+import '@quasar/extras/material-icons/material-icons.css'
+// Import Quasar css
+import 'quasar/src/css/index.sass'
 
 import App from './App.vue'
 import router from './router'
@@ -10,5 +17,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+
+app.use(Quasar, {
+  plugins: {},
+  lang: quasarLang,
+})
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,28 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+//import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueJsx()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls }
+    }),
+    vueJsx(),
+    quasar({
+      sassVariables: 'src/quasar-variables.sass',
+    }),
+  ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      //'@': path.resolve(__dirname, 'src'),
     }
-  }
+  },
+  publicDir: 'static',
+  build: {
+    outDir: 'public',
+  },
 })
