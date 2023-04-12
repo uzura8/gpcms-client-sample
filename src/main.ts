3667762220ca76a66ded1bcfaa381a2fa09bdd09
Shingo Yamada <uzuranoie@gmail.com>
2023-04-12 12:17:56 +0900

    Revert "Installed quasar"
    This reverts commit 36ae94327bacf69b1bcb42797984ef606ec1a224.

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,5 @@
 import { createApp } from 'vue'
-import { Quasar } from 'quasar'
 import { createPinia } from 'pinia'
-import quasarLang from 'quasar/lang/ja'
-
-// Import icon libraries
-import '@quasar/extras/material-icons/material-icons.css'
-// Import Quasar css
-import 'quasar/src/css/index.sass'
 
 import App from './App.vue'
 import router from './router'
@@ -17,10 +10,5 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
-app.use(Quasar, {
-  plugins: {},
-  lang: quasarLang,
-})
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -4,28 +4,12 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
-//import path from 'path'
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls }
-    }),
-    vueJsx(),
-    quasar({
-      sassVariables: 'src/quasar-variables.sass',
-    }),
-  ],
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      //'@': path.resolve(__dirname, 'src'),
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  },
-  publicDir: 'static',
-  build: {
-    outDir: 'public',
-  },
+  }
 })
