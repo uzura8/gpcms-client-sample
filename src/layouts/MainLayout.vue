7655c5abf8cea9adcff40714dea5ba794bc5e8c3
Shingo Yamada <uzuranoie@gmail.com>
2023-04-13 22:26:42 +0900

    Implemented header and footer sample

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,24 +1,72 @@
 <template>
   <div class="flex flex-col h-screen">
-    <header class="bg-blue-200">
-      <div class="p-4 max-w-screen-xl mx-auto">Header</div>
+    <header class="flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-white text-sm py-4 dark:bg-gray-800">
+      <nav class="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between" aria-label="Global">
+        <div class="flex items-center justify-between">
+          <RouterLink
+            to="/"
+            class="flex-none text-xl font-semibold dark:text-white"
+          >GPCMS</RouterLink>
+          <div class="sm:hidden">
+            <button
+              @click="isOpen = !isOpen"
+              type="button"
+              :class="{ open:isOpen }"
+              class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
+              data-hs-collapse="#navbar-with-collapse"
+              aria-controls="navbar-with-collapse"
+              aria-label="Toggle navigation"
+            >
+              <svg class="hs-collapse-open:hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+              </svg>
+              <svg class="hs-collapse-open:block hidden w-4 h-4" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div
+          :class="isOpen ? 'open' : 'hidden'"
+          id="navbar-with-collapse"
+          class="basis-full grow sm:block"
+        >
+          <div class="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:pl-5">
+            <RouterLink
+              to="/"
+              exact
+              :aria-current="current === '/' ? 'page' : null"
+              class="font-medium text-gray-600"
+            >Top</RouterLink>
+            <RouterLink
+              to="/about"
+              exact
+              :aria-current="current === '/about' ? 'page' : null"
+              class="font-medium text-gray-600"
+              aria-current="page"
+            >About</RouterLink>
+          </div>
+        </div>
+      </nav>
     </header>
+
     <div class="flex-1">
-      <div class="md:grid md:grid-cols-12 h-full max-w-screen-xl mx-auto">
+      <div class="md:grid md:grid-cols-12 h-full max-w-[85rem] mx-auto">
         <main class="md:col-span-9 lg:col-span-10 p-4">
-          <nav>
-            <RouterLink to="/">Home</RouterLink>
-            <RouterLink to="/about">About</RouterLink>
-            <RouterView />
-          </nav>
+          <RouterView />
         </main>
-        <aside class="md:col-span-3 lg:col-span-2 bg-blue-100 p-4">
+        <aside class="md:col-span-3 lg:col-span-2 p-4">
           Side
         </aside>
       </div>
     </div>
-    <footer class="bg-blue-200">
-      <div class="p-4 max-w-screen-xl mx-auto">Footer</div>
+
+    <footer class="mt-auto w-full max-w-[85rem] py-10 px-4 sm:px-6 lg:px-8 mx-auto">
+      <div class="text-center">
+        <div class="mt-3">
+          <p class="text-gray-500">© Preline. 2022 Htmlstream. All rights reserved.</p>
+        </div>
+      </div>
     </footer>
   </div>
 </template>
@@ -26,5 +74,23 @@
 <script lang="ts">
 import { defineComponent } from 'vue'
 
-export default defineComponent({})
+export default defineComponent({
+  data() {
+    return {
+      isOpen: false,
+    }
+  },
+
+  computed: {
+    current() {
+      return this.$route.path
+    },
+  }
+})
 </script>
+
+<style scoped>
+.router-link-exact-active {
+  color: #3B82F6;
+}
+</style>
