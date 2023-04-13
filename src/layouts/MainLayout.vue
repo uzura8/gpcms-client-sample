19e8168227000f2d258a3f7ecc0193218835ee8c
Shingo Yamada <uzuranoie@gmail.com>
2023-04-13 20:56:05 +0900

    Implemented basic layout

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,16 +1,26 @@
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="flex flex-col h-screen">
+    <header class="bg-blue-200">
+      <div class="p-4 max-w-screen-xl mx-auto">Header</div>
+    </header>
+    <div class="flex-1">
+      <div class="md:grid md:grid-cols-12 h-full max-w-screen-xl mx-auto">
+        <main class="md:col-span-9 lg:col-span-10 p-4">
+          <nav>
+            <RouterLink to="/">Home</RouterLink>
+            <RouterLink to="/about">About</RouterLink>
+            <RouterView />
+          </nav>
+        </main>
+        <aside class="md:col-span-3 lg:col-span-2 bg-blue-100 p-4">
+          Side
+        </aside>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+    <footer class="bg-blue-200">
+      <div class="p-4 max-w-screen-xl mx-auto">Footer</div>
+    </footer>
+  </div>
 </template>
 
 <script lang="ts">
